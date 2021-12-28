8774cbddf4be6a7a67758a4fd5e27f32ca6d867b
Marjoree <65003463+mfargas@users.noreply.github.com>
2021-12-28 15:34:47 -0800

    Update response to prompt

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const shortid = require('shortid');
 const app = express();
 
 // Basic Configuration
@@ -12,7 +13,7 @@ mongoose.connect(process.env.URI);
 let schema = new mongoose.Schema({ 
   original_url: String,
   short_url: String,
-  shrtID: String
+  trailing_id: String
 });
 let Url = mongoose.model('Url', schema);
 
@@ -25,24 +26,36 @@ app.use('/public', express.static(`${process.cwd()}/public`));
 app.get('/', (req, res) => { res.sendFile(process.cwd() + '/views/index.html') });
 
 app.post('/api/shorturl/', jsonParser, (req, res) => {
+  let trailingID = shortid.generate();
   let originalUrl = req.body['url'];
-  let short_url = originalUrl + '22135';
+  let shortUrl = __dirname + '/api/shorturl/' + trailingID;
   let newUrl = new Url({
-    original_url: originalUrl
+    original_url: originalUrl,
+    short_url: shortUrl,
+    trailing_id: trailingID
   });
 
   console.log(newUrl);
-  res.json(newUrl);
+  newUrl.save((err, doc) => {
+    if(err) console.log(err);
+    console.log('Success :)', newUrl);
+    res.json(newUrl);
+  })
 });
+
+app.get('/api/shorturl/:trailing_id', (req, res) => {
+  let trailingID = req.params.trailing_id;
+  console.log(trailingID);
+  res.json({
+    'id': trailingID
+  })
+})
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
 
 // TEST CASES TO PASS
-
-// You can POST a URL to / api / shorturl and get a JSON response
-//  with original_url and short_url properties.Here's an example: { original_url : 'https://freeCodeCamp.org', short_url : 1}
 
 // When you visit / api / shorturl / <short_url>, you will be redirected to the original URL.
 
