9eb91564476dca79024d8ac2eee3c42d43746bb3
Marjoree <65003463+mfargas@users.noreply.github.com>
2021-12-28 14:41:22 -0800

    Update answer to prompt

--- server.js
+++ server.js
@@ -7,41 +7,39 @@ const app = express();
 
 // Basic Configuration
 const port = process.env.PORT || 4000;
+var jsonParser = bodyParser.json();
+mongoose.connect(process.env.URI);
+let schema = new mongoose.Schema({ 
+  original_url: String,
+  short_url: String,
+  shrtID: String
+});
+let Url = mongoose.model('Url', schema);
 
 app.use(cors());
 
+app.use(bodyParser.urlencoded({ extended: false }));
+
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', (req, res) => {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get('/', (req, res) => { res.sendFile(process.cwd() + '/views/index.html') });
+
+app.post('/api/shorturl/', jsonParser, (req, res) => {
+  let originalUrl = req.body['url'];
+  let short_url = originalUrl + '22135';
+  let newUrl = new Url({
+    original_url: originalUrl
+  });
+
+  console.log(newUrl);
+  res.json(newUrl);
 });
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
 });
 
-mongoose.connect(process.env.URI);
-
-let schema = new mongoose.Schema({ url: 'string' });
-let Url = mongoose.model('Url', schema);
-
-let resObject = {};
-
-app.post('/api/shorturl/new', bodyParser.urlencoded({ extended: false }), (req, res) => {
-  let original_url = req.body['url'];
-  // let short_url = original_url;
-  console.log(req.body);
-  resObject['original_url']= original_url;
-
-  // await Url.create({ url: 'test' }, (err, data) => {
-  //   res.json({ created: true })
-  // })
-  res.json(resObject);
-})
-
 // TEST CASES TO PASS
-
-// You should provide your own project, not the example URL.
 
 // You can POST a URL to / api / shorturl and get a JSON response
 //  with original_url and short_url properties.Here's an example: { original_url : 'https://freeCodeCamp.org', short_url : 1}
