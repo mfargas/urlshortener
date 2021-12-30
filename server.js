de756d251129b1445802d4b0061cec7190a6f006
Marjoree <65003463+mfargas@users.noreply.github.com>
2021-12-30 12:09:00 -0800

    test inf redirect

--- server.js
+++ server.js
@@ -1,60 +1,77 @@
 require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
+const dns = require('dns');
+const urlparser = require('url');
 const mongoose = require('mongoose');
 const cors = require('cors');
 const shortid = require('shortid');
-const validUrl = require('valid-url');
 const app = express();
 
 // Basic Configuration
-const port = process.env.PORT || 4000;
 var jsonParser = bodyParser.json();
+const port = process.env.PORT || 4000;
 mongoose.connect(process.env.URI);
+
 let schema = new mongoose.Schema({ 
   original_url: String,
   short_url: String,
   trailing_id: String
 });
+
 let Url = mongoose.model('Url', schema);
 
 app.use(cors());
 
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
+// app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
 app.get('/', (req, res) => { res.sendFile(process.cwd() + '/views/index.html') });
 
 app.post('/api/shorturl/', jsonParser, (req, res) => {
-  const urlRegEx = new RegExp(/(?<=(http|https):\/\/)[^\/]+/);
   let originalUrl = req.body.url;
   let trailingID = shortid.generate();
   let shortUrl = '/api/shorturl/' + trailingID;
-  if (originalUrl.match(urlRegEx)){
-    let newUrl = new Url({
-      original_url: originalUrl,
-      short_url: shortUrl,
-      trailing_id: trailingID
-    });
-    console.log(newUrl);
-    newUrl.save((err, doc) => {
-      if (err) console.log(err);
-      res.json(newUrl);
-    });
-  } else {
-    res.status(400).json({ error: 'invalid url' });
-  }
+
+  dns.lookup(urlparser.parse(originalUrl).hostname, (error, address)=>{
+    if(!address){
+      res.json({ error: 'invalid url' });
+    } else {
+      let newUrl = new Url({
+        original_url: originalUrl,
+        short_url: shortUrl,
+        trailing_id: trailingID
+      });
+      console.log(newUrl);
+      newUrl.save((err, data) => {
+        if (err) console.log(err);
+        res.json({
+          original_url: data.original_url,
+          short_url: data.short_url
+        });
+      });
+    }
+  });
 });
 
 app.get('/api/shorturl/:trailing_id',  (req, res) => {
-  let generatedID = req.params.trailing_id;
+  let reqURL = req.params.original_url;
+  let reqID = req.params.trailing_id;
+  Url.find({ short_url: req.params.short_url }, (err, data)=>{
+    if(err) console.log(err);
+    if(!data){
+      res.json({ error: 'invalid url' });
+    } else {
+      return res.redirect(302, data[0].original_url)
+    }
+  })
+    // .then((foundURL)=>{
+    // let origUrl = foundURL[0];
+    // let shorty = foundURL[1]
+    // res.redirect(shorty.origUrl);}
   
-  Url.find({trailing_id: generatedID}).then((foundURL)=>{
-    let urlForRedir = foundURL[0];
-    console.log(urlForRedir);
-    res.redirect(urlForRedir.original_url);
-  });
 });
 
 app.listen(port, () => {
@@ -63,5 +80,7 @@ app.listen(port, () => {
 
 // TEST CASES TO PASS
 
-//   If you pass an invalid URL that doesn't follow the valid http://www.example.com format, the JSON response will contain 
-//   {error: 'invalid url' }
+//  When you visit /api/shorturl/<short_url>, you will be redirected to the original URL.
+
+// If you pass an invalid URL that doesn't follow the valid http://www.example.com format, 
+//  the JSON response will contain { error: 'invalid url' }
